d81d4f9924492bd66235b3a3e3b240f752a96454
junbeom95-lee <junbeom95@gmail.com>
2025-10-13 17:54:49 +0900

    1-5 실습 추가

--- src/chapter1/io/Ex.java
+++ src/chapter1/io/Ex.java
@@ -1,4 +1,21 @@
 package chapter1.io;
 
-public class Ex {
+import java.util.Scanner;
+
+public class Ex {   //1-5 실습해보기
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 사용자로부터 이름을 입력받기
+        System.out.print("이름을 입력해주세요: ");
+        String name = scanner.nextLine();
+
+        // TODO: 사용자로부터 나이를 입력받기
+        System.out.print("나이를 입력해주세요: ");
+        int age = scanner.nextInt();
+
+        // TODO: 입력받은 값 출력
+        System.out.print("출력 결과:\n이름: " + name +"\n나이: " + age);
+    }
 }
--- src/chapter1/io/Main.java
+++ src/chapter1/io/Main.java
@@ -1,4 +1,46 @@
 package chapter1.io;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        //출력
+        //println 사용
+        System.out.println( "Hello");
+        System.out.println( "Java");
+
+        //print 사용
+        System.out.print("안녕");
+        System.out.print("자바");
+
+        //개행문자 포함 출력
+        System.out.println("Hello\nWorld!");
+
+        //입력(Scanner)
+        Scanner scanner = new Scanner(System.in);
+
+        //정수형 데이터를 정수형 박스 (a)에 넣은거임
+        int a = 1;
+
+        //문자열 입력 받기
+        System.out.print("좋아 하는 문장을 입력하세요: ");
+        String sentence = scanner.nextLine();
+        System.out.println("sentence = " + sentence);
+
+        //정수형(int, long) 입력받기
+        System.out.print("정수(int)를 입력하세요: ");
+        int intBox = scanner.nextInt();
+        System.out.println("intBox = " + intBox);
+
+        System.out.print("정수(long)를 입력하세요: ");
+        long longBox = scanner.nextLong();
+        System.out.println("intBox = " + longBox);
+
+        //소수점
+        System.out.print("소수점(double)를 입력하세요: ");
+        double doubleBox = scanner.nextDouble();
+        System.out.println("doubleBox = " + doubleBox);
+    }
 }
