1cf5d6bbaf0b39eff5746d41ddd2251df2d91a63
junbeom95-lee <junbeom95@gmail.com>
2025-10-15 09:15:36 +0900

    2-4 강의 수강 chapter2.staticexample 강의 Main, Person

---
+++ src/chapter2/staticexample/Main.java
@@ -0,0 +1,24 @@
+package chapter2.staticexample;
+
+public class Main {
+    public static void main(String[] args) {
+        //static 변수, 메서드 활용
+        //Person은 프로그램 시작 시점에 Method Area에 올라와 있음
+        System.out.println("static 변수 활용: " + Person.population);
+        Person.printPopulation();
+
+        //인스턴스 멤버 활용
+        Person p1 = new Person();
+        Person p2 = new Person();
+
+        //인스턴스 변수 활용
+        p1.name = "jblee";
+        p2.name = "Steve";
+
+        //인스턴스 메서드 활용
+        p1.printName();
+        p2.printName();
+
+        Person.printPopulation();
+    }
+}
