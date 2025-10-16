2735be966129ef0a277f3ca9bb8084d4a70088ae
junbeom95-lee <junbeom95@gmail.com>
2025-10-16 11:57:20 +0900

    2-6 강의 수강 chapter2.interfaceexample v2 패키지 생성 Config, Main

---
+++ src/chapter2/interfaceexample/v2/Config.java
@@ -0,0 +1,8 @@
+package chapter2.interfaceexample.v2;
+
+public interface Config {
+
+    //public static final로 선언됩니다.
+    int POPULATION = 100;
+
+}
---
+++ src/chapter2/interfaceexample/v2/Main.java
@@ -0,0 +1,7 @@
+package chapter2.interfaceexample.v2;
+
+public class Main {
+    public static void main(String[] agrs) {
+        System.out.println(Config.POPULATION);
+    }
+}
